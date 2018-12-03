2f107f8f823cd72a9dcb3715e1bcda31ca6ece44
eduoo <44001810+eduoo@users.noreply.github.com>
2018-12-03 20:28:59 +0300

    Add project files.

---
+++ App/Form1.cs
@@ -0,0 +1,29 @@
+﻿using App.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Service1Client service = new Service1Client();
+            int reg = service.register("username", "password", "email");
+            int log = service.login("username", "password");
+            service.Close();
+        }
+    }
+}
